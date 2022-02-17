3e5cea8e2b764ccb293ccfa80f2f4f6155d51452
TaylorAleseHarris <93309433+TaylorAleseHarris@users.noreply.github.com>
2022-02-17 05:37:26 -0700

    Add files via upload

---
+++ S2 - Project 1 - Week 1/Project 1/js/FI.js
@@ -0,0 +1,16 @@
+/*********************************************************************
+***
+*Original Author: Taylor Harris *
+*Date Created: 1/19/22 *
+*Version: 1 *
+*Date Last Modified: 1/21/22 *
+*Modified by: Taylor Harris *
+*Modification log:              *
+1/19/22 - Created js.
+1/21/22 - Added alert.
+***
+******************************************************************** */
+
+alert("This site's leader may alarm you. Move forward at your own risk.");
+
+
