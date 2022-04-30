599a6cb3ce1c665e2e36cdf1065b3d79bbd4cc84
TaylorAleseHarris <93309433+TaylorAleseHarris@users.noreply.github.com>
2022-04-30 17:28:54 -0600

    Add files via upload

---
+++ Taylor Harris Portfolio/assets/js/main.js
@@ -0,0 +1,142 @@
+/*********************************************************************
+**
+#Original Author: Taylor Harris #
+#Date Created: 4/20/22 #
+#Version: 1 #
+#Date Last Modified: 4/25/22 #
+#Modified by: Taylor Harris #
+#Modification log:              #
+4/20/22 - Added js for sidebar.
+4/22/22 - Added js for portfolio section.
+4/23/22 - Fixed js for portfolio section.
+4/25/22 - Added js for scrollbar and nav.
+**
+******************************************************************** */
+
+
+
+/*========== SHOW SIDEBAR ==========*/
+const navMenu = document.getElementById('sidebar'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+/*===== SIDEBAR SHOW =====*/
+/* Validate if constant exists */
+if(navToggle) {
+    navToggle.addEventListener("click", () => {
+        navMenu.classList.add('show-sidebar')
+    })
+}
+
+/*===== SIDEBAR HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose) {
+    navClose.addEventListener("click", () => {
+        navMenu.classList.remove('show-sidebar')
+    })
+}
+
+
+
+/*========== MIXITUP FILTER ==========*/
+let mixerPortfolio = mixitup('.work__container', {
+    selectors: {
+        target: '.work__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
+
+
+
+/*========== LINK ACTIVE WORK ==========*/
+const linkWork = document.querySelectorAll('.work__item')
+
+function activeWork() {
+    linkWork.forEach(l=> l.classList.remove('active-work'))
+    this.classList.add('active-work')
+}
+
+linkWork.forEach(l=> l.addEventListener("click", activeWork))
+
+
+
+/*========== PORTFOLIO POPUP ==========*/
+document.addEventListener("click", (e) => {
+    if(e.target.classList.contains("work__button")) {
+        togglePortfolioPopup();
+        portfolioItemDetails(e.target.parentElement);
+    }
+})
+
+function togglePortfolioPopup() {
+    document.querySelector(".portfolio__popup").classList.toggle("open");
+}
+
+document.querySelector(".portfolio__popup-close").addEventListener("click", togglePortfolioPopup)
+
+
+function portfolioItemDetails(portfolioItem) {
+    var codeThin = "assets/images/iconmonstr-code-thin-240.png"
+    // document.querySelector(".pp__thumbnail img").src = portfolioItem.querySelector(".portfolio__popup-img").src;   //THIS IS THE ORIGINAL
+    document.querySelector(".pp__thumbnail img").src = codeThin;   //THIS IS THE NOT ORIGINAL
+    // document.querySelector(".portfolio__popup-img img").src = portfolioItem.querySelector(".work__img").src;         //TEST
+    // document.querySelector(".portfolio__popup-subtitle span").innerHTML = portfolioItem.querySelector(".work__title").innerHTML;
+    document.querySelector(".portfolio__popup-body").innerHTML = portfolioItem.querySelector(".portfolio__item-details").innerHTML;
+}
+
+
+/*========== INPUT ANIMATION ==========*/
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+    let parent = this.parentNode;
+    parent.classList.add("focus");
+}
+
+function blurFunc() {
+    let parent = this.parentNode;
+    if(this.value == "") {
+        parent.classList.remove("focus");
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener("focus", focusFunc);
+    input.addEventListener("blur", blurFunc);
+})
+
+/*========== SCROLL SECTIONS ACTIVE LINK ==========*/
+//get all sections that have an id defined
+const sections = document.querySelectorAll("section[id]");
+
+//add an event listener listening for scroll
+window.addEventListener("scroll", navHighlighter);
+
+function navHighlighter()
+{
+    //get current scroll position
+    let scrollY = window.pageYOffset;
+    //Now we loop through sections to get height, top and ID values for each
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50,
+        sectionId = current.getAttribute("id");
+        /* - If our current scroll position enters the space where current section on screen is, add.active class to
+        correcsponding navigation link, else remove it
+        - To know which link needs an active class, we use sectionId variable we are getting while looping through
+        sections as a selector */
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight)
+        {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add("active-link")
+        }
+        else
+        {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove("active-link")
+        }
+    })
+}
+
+
+/*========== SHOW SCROLL UP ==========*/
